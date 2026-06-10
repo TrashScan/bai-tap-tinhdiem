508f782e48327ac18555e591e572e2b370597bbb
Thanh Hien <thanhhien260105@gmail.com>
2026-06-10 20:37:54 +0700

    Add GPA calculation function in main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+def tinh_diem_gpa(diem_so):
+    if diem_so >= 8.5:
+        return 4.0
+    elif diem_so >= 8.0:
+        return 3.5
+    elif diem_so >= 7.0:
+        return 3.0
+    elif diem_so >= 6.5:
+        return 2.5
+    elif diem_so >= 5.5:
+        return 2.0
+    elif diem_so >= 5.0:
+        return 1.5
+    elif diem_so >= 4.0:
+        return 1.0
+    else:
+        return 0.0
+
+print("Điểm GPA hệ 4 là:", tinh_diem_gpa(8.5))
